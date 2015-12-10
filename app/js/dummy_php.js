5e9033478b71a484bbd27d051d5d25602200fccb
Ben Chartoff <bchartoff@gmail.com>
2015-12-10 14:43:40 -0500

    update sampel node ids

--- app/js/dummy_php.js
+++ app/js/dummy_php.js
@@ -1,7 +1,7 @@
 var settings = {
-	"table_feed": "http://tpctables-stg.urban.org/node/48448/table_feed",
-	"default_sheet": 48451,
-	"default_table": 48449
+	"table_feed": "http://tpctables-stg.urban.org/node/48418/table_feed",
+	"default_sheet": 48421,
+	"default_table": 48419
 }
 
 renderTableBook(true, settings.table_feed, settings.default_sheet, settings.default_table)
